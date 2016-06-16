2071bf70539b14c22bf8c8914b8af01b467dc7b6
NellySaavedra <oficinadenellys.sistemas@gmail.com>
2016-06-16 09:57:52 -0500

    Completado

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
-$('.Panel').hide();
-$('#BInicio').click(function(){
-        $('.Panel').slideToggle('slow');
+  $('#BInicio').click(function(){
+      window.location.reload();  
    });
 });
 
 var Alternancia =1;
-var b1="b1",
-    b2="b2",
-    b3="b3",
-    b4="b4",
-    b5="b5",
-    b6="b6",
-    b7="b7",
-    b8="b8",
-    b9="b9";
-    
+var casilla1="casilla1",
+    casilla2="casilla2",
+    casilla3="casilla3",
+    casilla4="casilla4",
+    casilla5="casilla5",
+    casilla6="casilla6",
+    casilla7="casilla7",
+    casilla8="casilla8",
+    casilla9="casilla9";
+
 function actions(boton){
  if(boton.className=="buttons"){
   boton.className="clicked";
@@ -29,51 +28,62 @@ function actions(boton){
   boton.appendChild(document.createTextNode(text));
   Alternancia++;
   asignation(text,boton);
-  }else{
- }
+  }
 }
+var empate = 0;
 function asignation(text,boton){
-       if(b1==boton.name){
-        b1=text;
+       if(casilla1==boton.name){
+        casilla1=text;
+        empate++;
        }
-       else if(b2==boton.name){
-        b2=text;
+       else if(casilla2==boton.name){
+        casilla2=text;
+        empate++;
        }
-       else if(b3==boton.name){
-        b3=text;
+       else if(casilla3==boton.name){
+        casilla3=text;
+        empate++;
        }
-       else if(b4==boton.name){
-        b4=text;
+       else if(casilla4==boton.name){
+        casilla4=text;
+        empate++;
        }
-       else if(b5==boton.name){
-        b5=text;
+       else if(casilla5==boton.name){
+        casilla5=text;
+        empate++;
        }
-       else if(b6==boton.name){
-        b6=text;
+       else if(casilla6==boton.name){
+        casilla6=text;
+        empate++;
        }
-       else if(b7==boton.name){
-        b7=text;
+       else if(casilla7==boton.name){
+        casilla7=text;
+        empate++;
        }
-       else if(b8==boton.name){
-        b8=text;
+       else if(casilla8==boton.name){
+        casilla8=text;
+        empate++;
        }
-       else if(b9==boton.name){
-        b9=text;
+       else if(casilla9==boton.name){
+        casilla9=text;
+        empate++;
        }
  validation(text);
 }
+
 function validation(text){
- if((b1==b2 && b2==b3)||(b1==b5 && b5==b9)
-  ||(b3==b5 && b5==b7)||(b4==b5 && b5==b6)
-  ||(b7==b8 && b8==b9)||(b1==b4 && b4==b7)
-  ||(b2==b5 && b5==b8)||(b3==b6 && b6==b9)){
+ if((casilla1==casilla2 && casilla2==casilla3)||(casilla1==casilla5 && casilla5==casilla9)
+  ||(casilla3==casilla5 && casilla5==casilla7)||(casilla4==casilla5 && casilla5==casilla6)
+  ||(casilla7==casilla8 && casilla8==casilla9)||(casilla1==casilla4 && casilla4==casilla7)
+  ||(casilla2==casilla5 && casilla5==casilla8)||(casilla3==casilla6 && casilla6==casilla9)){
       var Alternancia=confirm("Felicidades! Has ganado! Da clic para volver a comenzar");
       if(Alternancia==true){
        window.location.reload();
       }
    }
-  if(Alternancia==true){
-    alert("Dds");
-  } 
+   else if(empate==9){
+    alert("ESTO FUE UN EMPATE. Volvamos a empezar");
+    window.location.reload();
+   }
 }
 

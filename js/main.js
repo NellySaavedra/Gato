0e065274d660bafd4191cf83a3037a1659f0ab94
NellySaavedra <oficinadenellys.sistemas@gmail.com>
2017-02-11 22:50:42 -0600

    Prueba de registro: Usuarios

--- js/main.js
+++ js/main.js
@@ -1,26 +1,59 @@
 $(document).ready(function(){
+    /* Menu */
+    if (window.location == window.parent.location) {
+      $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
+      $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
+      $('#fullscreen').attr('title', 'Back To Bootsnipp');
+    }    
+    $('#fullscreen').on('click', function(event) {
+        event.preventDefault();
+        window.parent.location =  $('#fullscreen').attr('href');
+    });
+    $('#fullscreen').tooltip();
+    $('.navbar-toggler').on('click', function(event) {
+      event.preventDefault();
+      $(this).closest('.navbar-minimal').toggleClass('open');
+    })
+
+  /*VISTAS*/  
     $("form").hide();
     $("#jugar").click(function(){
-        $("form").show();
-        $("img").hide();
-        $("#jugar").hide();
+      $("form").show();
+      $("img").hide();
+      $("#jugar").hide();
     });
-      $('#BInicio').click(function(){
+  /* */
+    $(".box-cat").hide();
+    $(".btnOk").click(function(){
+      $(".acceso").hide();
+      $(".box-cat").show();
+    });
+    $('#BInicio').click(function(){
       window.location.reload();  
-   });
+    });
 });
+function myFunction() {
+  var personA = document.getElementById("inputNombreA").value;
+  var personB = document.getElementById("inputNombreB").value;
+  if (personA != null && personB != null) {
+    document.getElementById("demoA").innerHTML 
+    document.getElementById("demoB").innerHTML =
+    personA + " y " + personB + "... pelea!!!";
+  }
+  if (personA == "" && personB == ""){
+    alert("Necesitamos dos jugadores para empezar");
+  }
+}
 
-$('#btnClick').click(function(){
-  var nombre = $('#inputNombre').val(); 
-  console.log('El usuario escribió: ' + nombre);
-
-});
 
 
+/*CONTADOR*/
+var contador = 0;
+var O = "X"; 
+var X = "O";
 
 
-
-
+/*JUEGO*/
 var Alternancia =1;
 var casilla1="casilla1",
     casilla2="casilla2",
@@ -37,9 +70,9 @@ function actions(boton){
   boton.className="clicked";
   var text;
   if(Alternancia%2==0){
-        text="X";
-  }else{
         text="O";
+  }else{
+        text="X";
   }
   boton.value=text;
   boton.appendChild(document.createTextNode(text));
@@ -92,32 +125,14 @@ function validation(text){
  if((casilla1==casilla2 && casilla2==casilla3)||(casilla1==casilla5 && casilla5==casilla9)
   ||(casilla3==casilla5 && casilla5==casilla7)||(casilla4==casilla5 && casilla5==casilla6)
   ||(casilla7==casilla8 && casilla8==casilla9)||(casilla1==casilla4 && casilla4==casilla7)
-  ||(casilla2==casilla5 && casilla5==casilla8)||(casilla3==casilla6 && casilla6==casilla9)){
-      var Alternancia=confirm("Felicidades! Has ganado! Da clic para volver a comenzar");
-      if(Alternancia==true){
-       window.location.reload();
-      }
-   }
+  ||(casilla2==casilla5 && casilla5==casilla8)||(casilla3==casilla6 && casilla6==casilla9)) {
+    var Alternancia=confirm("Felicidades! Has ganado! Da clic para volver a comenzar");
+    if(Alternancia==true){
+      window.location.reload();     
+    }
+  }
    else if(empate==9){
     alert("ESTO FUE UN EMPATE. Volvamos a empezar");
     window.location.reload();
    }
 }
-
-$(function () {
-    /* Menu */
-    if (window.location == window.parent.location) {
-        $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
-        $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
-        $('#fullscreen').attr('title', 'Back To Bootsnipp');
-    }    
-    $('#fullscreen').on('click', function(event) {
-        event.preventDefault();
-        window.parent.location =  $('#fullscreen').attr('href');
-    });
-    $('#fullscreen').tooltip();
-    $('.navbar-toggler').on('click', function(event) {
-    event.preventDefault();
-    $(this).closest('.navbar-minimal').toggleClass('open');
-  })
-});
